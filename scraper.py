b84fe434154d25c83c1af01f8b1266428ddb11c1
aliexer <aliexer.mayor@sourcemeridian.com>
2023-07-03 00:05:28 -0500

    changing website

---
+++ scraper.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# website to scraper.
+url = 'https://www.selenium.dev/'
+# webdriver for browser chrome.
+driver = webdriver.Chrome(executable_path='/chromedriver_mac64/chromedriver')
+driver.get(url)
+features = driver.find_elements(By.XPATH, '/html/body/div/main/section[2]/div/div')
+for name in features:
+    print(name.text)
+
+driver.close()
