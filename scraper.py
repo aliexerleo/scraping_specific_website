0507be2315694e69a1163cf8d00cea4768f41ef5
aliexer <aliexer.mayor@sourcemeridian.com>
2023-07-03 15:12:46 -0500

    fixing name script

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ url = 'https://www.selenium.dev/'
 driver = webdriver.Chrome(executable_path='/chromedriver_mac64/chromedriver')
 driver.get(url)
 features = driver.find_elements(By.XPATH, '/html/body/div/main/section[2]/div/div')
-for name in features:
-    print(name.text)
+for item in features:
+    print(item.text)
 
 driver.close()
